f1a5bf086f9982d6a54f516cc2aeb0e7cc54c528
Sachin Gurung <sachingurung@Sachins-MacBook-Pro.local>
2024-04-25 06:38:24 -0700

    Give an ID to the frontend to target it from backend.

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.practiceapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.myButton);
+
+        myButton.setText("Hello from Main");
+
+        ImageView myImageView = findViewById(R.id.myCarPicture);
+        myImageView.setImageResource(R.drawable.mountain);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
